384925a05a3330c8a20c2e99a7b1b55a2ca216bd
Austin Yeon <austinyeon@gmail.com>
2021-08-08 14:09:34 -0700

    started linkedList data structure file

---
+++ linkedLists.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor (data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insertFirst (data) {
+    this.head = new Node (data, this.head);
+    this.size++;
+  }
+}
+
+let ll = new LinkedList();
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+
+console.log(ll);
